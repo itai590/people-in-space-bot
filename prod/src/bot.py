69d9fea55fa67922515a7e081b3e81ab328e2531
Itai <itai590@gmail.com>
2023-07-27 23:12:41 +0300

    Add format_explanation

--- prod/src/bot.py
+++ prod/src/bot.py
@@ -9,12 +9,20 @@ from user import User
 from utilities import Utilities as Util
 
 BOT_TOKEN = os.environ.get("BOT_TOKEN")
+GET_USERS_TOKEN = os.environ.get("GET_USERS_TOKEN")
 USERS_FILENAME = "users.json"
 
 
 async def hello(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     hello_msg = "Welcome to people in space updates bot, a daily update will be sent every morning 08:00AM IL Time"
-    await update.message.reply_text(f'Hello {update.effective_user.first_name} {hello_msg}')
+    format_explanation = "Format Explanation\n \
+                        🚀   🪐  🌍    10 🧑‍🚀🧑‍🚀\n \
+                        Number of people in space\n \
+                        1.Jame James🇺🇸 - FLT Eng. - 120 \n \
+                        Name - Country - Role - Days in space\n"
+                        
+    ans = f'{hello_msg}\n{format_explanation}'
+    await update.message.reply_text(f'Hello {update.effective_user.first_name} {ans}')
 
 
 async def subscribe(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
@@ -24,6 +32,7 @@ async def subscribe(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     Logger.log_subscription_event_to_file(user)
     Util.add_user(user, USERS_FILENAME)
     await update.message.reply_text(f'Thanks {update.effective_user.first_name + " " + update.effective_user.last_name}". Your subscription has been set"')
+
 
 async def unsubscribe(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     user = User(update.effective_user.first_name, update.effective_user.last_name, user['username'], user['id'])
@@ -35,11 +44,16 @@ async def unsubscribe(update: Update, context: ContextTypes.DEFAULT_TYPE) -> Non
 
 
 async def users(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
-    pass
+    user = User(update.effective_user.first_name, update.effective_user.last_name, user['username'], user['id'])
+    Logger.log_user_call(user)
+    ans = Util._get_users(USERS_FILENAME)
+    await update.message.reply_text(f"{ans}")
 
 
 def bot():
     bot_logger = Logger()
+
+    get_users_command = f'getusers_{GET_USERS_TOKEN}'
 
     try:
         app = ApplicationBuilder().token(BOT_TOKEN).http_version("1.1").get_updates_http_version("1.1").build()
@@ -48,6 +62,7 @@ def bot():
         app.add_handler(CommandHandler("subscribe", subscribe))
         app.add_handler(CommandHandler("unsubscribe", unsubscribe))
         app.add_handler(CommandHandler("howmany", scrape))
+        app.add_handler(CommandHandler(get_users_command, users))
 
         app.run_polling()
 
